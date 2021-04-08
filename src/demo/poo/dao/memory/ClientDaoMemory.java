750890809b5668d907b319816e56ad0b45a2b0b8
gverissi <greg.verissimo@gmail.com>
2021-04-08 14:55:19 +0200

    Add Class ClientDaoMemory

---
+++ src/demo/poo/dao/memory/ClientDaoMemory.java
@@ -0,0 +1,39 @@
+package demo.poo.dao.memory;
+
+import demo.poo.dao.ClientDaoInterface;
+import demo.poo.entity.Client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClientDaoMemory implements ClientDaoInterface {
+
+    private static long id;
+
+    private final Map<Long, Client> clientMap = new HashMap<>();
+
+    @Override
+    public Client save(Client client) {
+        long id = client.getId();
+        if (!clientMap.containsValue(client)) {
+            id = getNewId();
+            client.setId(id);
+        }
+        clientMap.put(id, client);
+        return client;
+    }
+
+    private long getNewId() {
+        return ++id;
+    }
+
+    @Override
+    public Client findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void removeByName(String name) {
+
+    }
+}
