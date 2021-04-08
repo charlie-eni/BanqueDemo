f15b9b60a92f414e372cb041304eb11a737f1dab
gverissi <greg.verissimo@gmail.com>
2021-04-08 14:46:13 +0200

    Add Class Client

---
+++ src/demo/poo/entity/Client.java
@@ -0,0 +1,35 @@
+package demo.poo.entity;
+
+public class Client {
+
+    private long id;
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
